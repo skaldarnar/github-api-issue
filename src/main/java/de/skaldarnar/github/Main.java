d83cf5f3d77bdc451d7869b0efcf6e563ccaa660
Tobias Nett <skaldarnar@googlemail.com>
2020-03-27 16:51:22 +0100

    MWE for github.com/github-api/github-api/issues/751

--- src/main/java/de/skaldarnar/github/Main.java
+++ src/main/java/de/skaldarnar/github/Main.java
@@ -3,9 +3,21 @@
  */
 package de.skaldarnar.github;
 
+import java.io.IOException;
+
+import org.kohsuke.github.GHOrganization;
+import org.kohsuke.github.GitHub;
+
 public class Main {
 
     public static void main(String[] args) {
-        
+        try {
+            GitHub github = GitHub.connectAnonymously();
+            GHOrganization movingBlocks = github.getOrganization("MovingBlocks");
+      
+            System.out.println(movingBlocks.toString());
+          } catch (IOException e) {
+            e.printStackTrace();
+          }
     }
 }
